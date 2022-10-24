dfaf633d7b84470bffc7842b6857190a334ee048
GuessMF <segas95@yandex.ru>
2022-10-24 17:47:19 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 function App() {
   return (
     <div>
-      <h1>
-        hello<span>world</span>
-      </h1>
+      <div className="header">
+        <div className="header-name">Название</div>
+        <div className="header-menu"></div>
+        <div className="header-search-cart"></div>
+      </div>
     </div>
   );
 }
