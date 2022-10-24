062aafdb628a651b768b1407c2527c7756bc28e1
GuessMF <segas95@yandex.ru>
2022-10-24 19:31:23 +0300

    master

--- src/App.js
+++ src/App.js
@@ -2,9 +2,59 @@ function App() {
   return (
     <div>
       <div className="header">
-        <div className="header-name">Название</div>
-        <div className="header-menu"></div>
-        <div className="header-search-cart"></div>
+        <div className="header-name">
+          <h1>Apple-shop</h1>
+        </div>
+        <div className="header-menu">
+          <li>All products</li>
+          <li>Solutions</li>
+          <li>About</li>
+          <li>Support</li>
+        </div>
+        <div className="header-search-cart">
+          <li>
+            <img width={30} height={30} src="/img/search-2.png" />
+          </li>
+          <li>
+            <img width={40} height={30} src="/img/Cart.png" />
+          </li>
+        </div>
+      </div>
+
+      <div className="slider">
+        <div className="slider-main-info">
+          <h2>The new phones are here take a look.</h2>
+          <p>
+            info info info info info info info info info info info info info
+            info info info info info
+          </p>
+          <button className="slider-main-button">Explore</button>
+        </div>
+        <img width={500} height={400} src="/img/slider-main.png" />
+      </div>
+      <div className="main-container">
+        <h3>Все товары</h3>
+        <div className="content">
+          <div className="iphone-12">
+            <img width={100} height={120} src="/img/iphone-12.jpeg" />
+            <p>Iphone 12 64gb</p>
+            <span>
+              {" "}
+              49999 P
+              <img width={25} height={15} src="/img/Cart.png" />
+            </span>
+          </div>
+
+          <div className="iphone-12">
+            <img width={100} height={120} src="/img/iphone-12.jpeg" />
+            <p>Iphone 12 64gb</p>
+            <span>
+              {" "}
+              49999 P
+              <img width={25} height={15} src="/img/Cart.png" />
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
