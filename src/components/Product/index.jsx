e36fe34b6a2ba03e6e523f9999d0aaa1f51cba3c
GuessMF <segas95@yandex.ru>
2022-11-05 18:31:45 +0300

    dxdexeedx xdxexffexd

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import styles from "./Product.module.scss";
 
 export default function Product(props) {
+  const [add, setAdd] = React.useState(false);
+
+  const onClickButton = () => {
+    setAdd(!add);
+    console.log("eeeee");
+  };
+
   return (
     <div className={styles.product}>
       <img width={110} height={130} src={props.imgURL} alt="prodImg" />
@@ -10,7 +17,13 @@ export default function Product(props) {
         <p className={styles.productMemory}>{props.memory}</p>
         <p className={styles.productPrice}>
           Цена: <b>{props.price}</b>{" "}
-          <img width={25} height={15} src="/img/Cart.png" alt="cart" />
+          <img
+            width={10}
+            height={10}
+            onClick={onClickButton}
+            src={add ? "/img/added.png" : "/img/plus.png"}
+            alt="add"
+          />
         </p>
       </div>
     </div>
